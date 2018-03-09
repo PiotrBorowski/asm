6177a73e81ba7cae79eda1b3f394e09e0e8cc404
PiotrBorowski <234996@student.pwr.edu.pl>
2018-03-09 13:48:34 +0100

    cos nie wypisuje

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-all: hello clean
+all: hello writeText compareText clean
 
 hello:	hello.o
 	ld -o hello hello.o
 
 hello.o:	hello.s
 		as -o hello.o hello.s
+
+writeText:	 writeText.o
+	  	 ld -o writeText writeText.o
+
+writText.o: 	writeText.s
+		as -o writeText.o writeText.s
 
 clean:
 	rm hello.o
--- writeText.s
+++ writeText.s
@@ -21,19 +21,41 @@ EXIT_SUCCESS = 0
 .global _start
 _start:
 
+wypiszmsg:
+	mov $SYSWRITE, %eax
+	mov $STDOUT, %edi
+	mov $msg, %esi
+	mov $msg_len, %edx
+	int $0x80
+
 wczytaj:
 	mov $SYSREAD, %eax
 	mov $STDIN, %ebx
 	mov $buf, %esi
 	mov $buf_len, %edx
 	int $0x80
+	mov %eax, %ebx
+
+wypiszmsg2:
+	mov $SYSWRITE, %eax
+        mov $STDOUT, %edi
+        mov $msg2, %esi
+        mov $msg2_len, %edx
+	int $0x80
 
 wypisz:
-        mov $SYSWRITE, %eax
-        mov $STDOUT, %ebx
-        mov $msg, %ecx
-        mov $msg_len, %edx
+	mov $SYSWRITE, %eax
+        mov $STDOUT, %edi
+        mov $buf, %esi
+        mov %ebx, %edx
         int $0x80
+
+wypisznewline:
+	mov $SYSWRITE, %eax
+        mov $STDOUT, %edi
+        mov $newline, %esi
+        mov $newline_len, %edx
+	int $0x80	
 
         mov $SYSEXIT, %eax
         mov $EXIT_SUCCESS, %ebx
