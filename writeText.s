69be0151ae3ee0163210035775bdfce6ee3af10f
PiotrBorowski <234996@student.pwr.edu.pl>
2018-03-09 14:14:19 +0100

    cos juz dziala

--- writeText.s
+++ writeText.s
@@ -1,14 +1,10 @@
-.text
+.data
 msg: .ascii "Write text (5): \n"
 msg_len = . - msg
-
 msg2: .ascii "Written text: "
 msg2_len = . - msg2
-
 newline: .ascii "\n"
 newline_len = . - newline
-
-.data
 buf: .ascii "     "
 buf_len = . - buf
 SYSREAD = 3
@@ -18,36 +14,36 @@ STDIN = 0
 SYSEXIT = 1
 EXIT_SUCCESS = 0
 
+.text
 .global _start
 _start:
 
 wypiszmsg:
 	mov $SYSWRITE, %eax
-	mov $STDOUT, %edi
-	mov $msg, %esi
+	mov $STDOUT, %ebx
+	mov $msg, %ecx
 	mov $msg_len, %edx
 	int $0x80
 
 wczytaj:
 	mov $SYSREAD, %eax
 	mov $STDIN, %ebx
-	mov $buf, %esi
+	mov $buf, %ecx
 	mov $buf_len, %edx
 	int $0x80
-	mov %eax, %ebx
 
 wypiszmsg2:
 	mov $SYSWRITE, %eax
-        mov $STDOUT, %edi
-        mov $msg2, %esi
+        mov $STDOUT, %ebx
+        mov $msg2, %ecx
         mov $msg2_len, %edx
 	int $0x80
 
 wypisz:
 	mov $SYSWRITE, %eax
-        mov $STDOUT, %edi
-        mov $buf, %esi
-        mov %ebx, %edx
+        mov $STDOUT, %ebx
+        mov $buf, %ecx
+        mov %buf_len, %edx
         int $0x80
 
 wypisznewline:
