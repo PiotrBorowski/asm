497c713feabe31f28f720713617cd01e639ca90f
PiotrBorowski <234996@student.pwr.edu.pl>
2018-03-10 11:19:11 +0100

    powinno byc na %al, cmp i jne ALE NIE JEST

---
+++ compareText.s
@@ -0,0 +1,64 @@
+.data
+msg: .ascii "Write text (3): \n"
+msg_len = . - msg
+msg2: .ascii "Same text! "
+msg2_len = . - msg2
+msg3: .ascii "Not the same"
+msg3_len = . - msg3
+buf: .ascii "   "
+buf_len = . - buf
+text: .ascii "ASM"
+text_len = . - text
+SYSREAD = 3
+SYSWRITE = 4
+STDOUT = 1
+STDIN = 0
+SYSEXIT = 1
+EXIT_SUCCESS = 0
+
+
+.text
+.global _start
+_start:
+
+wypiszmsg:
+        mov $SYSWRITE, %eax
+	mov $STDOUT, %ebx
+	mov $msg, %ecx
+	mov $msg_len, %edx
+	int $0x80
+
+wczytaj:
+	mov $SYSREAD, %eax
+	mov $STDIN, %ebx
+	mov $buf, %ecx
+	mov $buf_len, %edx
+	int $0x80
+
+porownaj:
+	mov $text, %esi
+	mov $buf, %edi
+	mov $buf_len, %ecx
+	cld
+	repe cmpsb
+	jecxz rowne
+
+nierowne: 
+        mov $SYSWRITE, %eax
+        mov $STDOUT, %ebx
+        mov $msg3, %ecx
+        mov $msg3_len, %edx
+        int $0x80
+	jmp exit
+
+rowne: 
+        mov $SYSWRITE, %eax
+        mov $STDOUT, %ebx
+        mov $msg2, %ecx
+        mov $msg2_len, %edx
+        int $0x80
+
+exit: 
+	mov $SYSEXIT, %eax
+	mov $EXIT_SUCCESS, %ebx
+	int $0x80
