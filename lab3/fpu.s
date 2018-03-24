cb12fd95064c80eab26462ce01c81665c09c2e0e
PiotrBorowski <234996@student.pwr.edu.pl>
2018-03-24 11:27:48 +0100

    fpu wyjatki

---
+++ lab3/fpu.s
@@ -0,0 +1,50 @@
+.code32
+
+.data
+
+l1: .float 14.12
+
+l2: .float -13.5
+
+cw: .short 0
+
+SYSEXIT = 1
+EXIT_SUCCESS = 0
+
+.text
+.global _start
+_start:
+
+pluszero:
+fld l1
+fldz
+fmulp
+
+minuszero:
+fld l2
+fldz
+fmulp
+
+plusinf:
+fldz
+fld l1
+fdivp
+
+minusinf:
+fldz
+fld l2
+fdivp
+
+NaN1:
+fld l2
+fsqrt
+
+NaN2:
+fldz
+fldz
+fdivp
+
+exit:
+mov $SYSEXIT, %eax
+mov $EXIT_SUCCESS, %ebx
+int $0x80
