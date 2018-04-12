dbf9e0f14408401c6f5ef01c5f72ddd70df95324
PiotrBorowski <234996@student.pwr.edu.pl>
2018-04-12 11:30:31 +0200

    dodawanie

--- lab4/main.c
+++ lab4/main.c
@@ -10,11 +10,54 @@ struct Vector{
 
 struct Vector Array[2048];
 
+// 4 * 2048 = 8192 liczb
 
+void generate()
+{
+    for(int i = 0; i < 2048; i++)
+    {
+        Array[i].a = rand()%100000 + 1; 
+        Array[i].b = rand()%100000 + 1; 
+        Array[i].c = rand()%100000 + 1; 
+        Array[i].d = rand()%100000 + 1; 
+    }
+}
 
+void printVector(struct Vector vec){
+    printf("%d %d %d %d\n",vec.a, vec.b,vec.c,vec.d);
+} 
 
+double ADD_SIMD(int numberOfNumbers)
+{   
+    double Time = 0;
+    struct Vector x, y, result;
 
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < numberOfNumbers / 4; j+=2)
+        {
+            x = Array[j];
+            y = Array[j+1];
 
+            clock_t start = clock();
+            asm(
+                "movups %1, %%xmm0 \n"
+                "movups %2, %%xmm1 \n"
+                "addps %%xmm1, %%xmm0 \n"
+                "movups %%xmm0, %0 \n"
+                : "=g"(result)
+                : "g"(x), "g"(y)
+            );
+            clock_t stop = clock();
+            Time += (double) (stop - start) / CLOCKS_PER_SEC;
+           // printVector(x);
+           // printVector(y);
+           // printVector(result);
+        }
+    }
+    Time /= 10.0;
+    return Time;
+}
 
 int main(){
     srand(time(NULL));
@@ -24,6 +67,9 @@ int main(){
         exit(2);
     }
 
+    generate();
+
+    printf("%lf",ADD_SIMD(8192));
 
 
     return 0;
