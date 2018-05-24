19ce0abfb0b1676e6618744caeacfd96826bfac4
PiotrBorowski <234996@student.pwr.edu.pl>
2018-05-24 11:56:00 +0200

    final

--- lab5/main.c
+++ lab5/main.c
@@ -97,13 +97,41 @@ float countDet(struct Vector mat[4])
         //(0,0,2,1)
         //(0,0,2,1)
         //(0,0,0,0)
+        "movups (%2), %%xmm0\n"
+        "movups (%3), %%xmm1\n"
+
+        "shufps $0b00000000, %%xmm0, %%xmm0\n"
+        "shufps $0b00001001, %%xmm1, %%xmm1\n"
+        "mulps %%xmm0, %%xmm1\n"
+        //wynik w xmm1
+
+        "movups (%2), %%xmm0\n"
+        "movups (%3), %%xmm4\n"
+
+        "shufps $0b00000000, %%xmm4, %%xmm4\n"
+        "shufps $0b00001001, %%xmm0, %%xmm0\n"
+        "mulps %%xmm0, %%xmm4\n"
+        //wynik w xmm4
+
+        "subps %%xmm1, %%xmm4\n"
+
+        //mnozenie C razy D wynik w xmm4
+        "mulps %%xmm2, %%xmm4\n"
+
+        //wynik
+        "addps %%xmm3, %%xmm4\n"
+        "movups %%xmm4, (%4)\n" //do temp
 
         :
         : "r" (mat[0].number), "r" (mat[1].number),  "r" (mat[2].number),  "r" (mat[3].number), "r" (temp.number)  
     );
 
-
+    return temp.number[0] + temp.number[1] + temp.number[2] + temp.number[3];
 }
+
+void printVector(struct Vector vec){
+    printf("%f %f %f %f\n",vec.number[0], vec.number[1],vec.number[2],vec.number[3]);
+} 
 
 int main()
 {
@@ -111,10 +139,10 @@ int main()
     float det;
 
     float data[16] = {
-        1,4,5,3,
-        5,2,7,1,
-        9,4,2,7,
-        9,9,3,9
+        1,0,2,0,
+        0,1,3,7,
+        1,0,1,0,
+        0,1,0,1
     };
 
     for(int i = 0, j = 0; i<4; i++, j+=4)
@@ -122,8 +150,10 @@ int main()
         matrix[i].number[0] = data[j];
         matrix[i].number[1] = data[j+1];
         matrix[i].number[2] = data[j+2];
-        matrix[i].number[3] = data[j+3];        
+        matrix[i].number[3] = data[j+3];   
+        printVector(matrix[i]);     
     }
 
-    printf("Wyznacznik = %f", countDet(matrix));
+
+    printf("Wyznacznik = %f\n", countDet(matrix));
 }
